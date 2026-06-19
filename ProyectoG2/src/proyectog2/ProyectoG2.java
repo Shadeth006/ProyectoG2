1106e84afb5db118256b3c9dc73148b558050501
MenaCV <114704782+MenaCV@users.noreply.github.com>
2026-06-18 22:20:34 -0600

    prueba jime

--- ProyectoG2/src/proyectog2/ProyectoG2.java
+++ ProyectoG2/src/proyectog2/ProyectoG2.java
@@ -15,6 +15,7 @@ public class ProyectoG2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.print("Prueba jime");
     }
     
 }
